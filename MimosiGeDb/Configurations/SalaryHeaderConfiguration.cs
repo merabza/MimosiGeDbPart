de315a6ee4a2273bf2e096612cd070319aa38ca8
merabza <merabza@gmail.com>
2025-05-11 21:14:31 +0400

    202505112114

---
+++ MimosiGeDb/Configurations/SalaryHeaderConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MimosiGeDb.Models;
+
+namespace MimosiGeDb.Configurations;
+
+public sealed class SalaryHeaderConfiguration : IEntityTypeConfiguration<SalaryHeader>
+{
+    public void Configure(EntityTypeBuilder<SalaryHeader> entity)
+    {
+        entity.HasKey(e => e.ShId);
+
+        entity.HasIndex(e => e.ShId);
+
+        entity.Property(e => e.ShId);
+        entity.Property(e => e.ShChargeDate).HasPrecision(0).HasComment("დარიცხვის თარიღი");
+        entity.Property(e => e.ShTransferDate).HasPrecision(0).HasComment("გადარიცხვის თარიღი");
+    }
+}
