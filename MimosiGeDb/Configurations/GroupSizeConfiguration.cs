6d45e35f3f26930f7bb93be692f415b82b0e3526
merabza <merabza@gmail.com>
2025-06-21 22:38:20 +0400

    202506212238

--- MimosiGeDb/Configurations/GroupSizeConfiguration.cs
+++ MimosiGeDb/Configurations/GroupSizeConfiguration.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Immutable;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using MimosiGeDb.Models;
 
