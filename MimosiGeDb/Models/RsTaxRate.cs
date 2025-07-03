dd332e389ed44b4dc97d9811d1d6af6a6c15eba8
merabza <merabza@gmail.com>
2025-07-04 01:29:06 +0400

    202507040128

--- MimosiGeDb/Models/RsTaxRate.cs
+++ MimosiGeDb/Models/RsTaxRate.cs
@@ -4,7 +4,7 @@ public sealed class RsTaxRate
 {
     public int Id { get; set; }
     public required string Code { get; set; }
-    public short? BenefCategoryId { get; set; }
+    public int BenefCategoryId { get; set; }
     public short? QuoteTypeId { get; set; }
     public string? TaxRate { get; set; }
     public RsBeneficiaryCategory? BenefCategory { get; set; }
