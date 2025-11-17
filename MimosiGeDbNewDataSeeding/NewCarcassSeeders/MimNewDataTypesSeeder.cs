683ff6aedb4a0c70204ef0224c705c4669dcf95b
merabza <merabza@gmail.com>
2025-11-18 00:10:39 +0400

    202511180004

--- MimosiGeDbNewDataSeeding/NewCarcassSeeders/MimNewDataTypesSeeder.cs
+++ MimosiGeDbNewDataSeeding/NewCarcassSeeders/MimNewDataTypesSeeder.cs
@@ -32,43 +32,45 @@ public sealed class MimNewDataTypesSeeder : MimDataTypesSeeder
         {
             //AcademicYear
             DataTypeFactory.Create("აკადემიური წლები", "აკადემიური წელი", "აკადემიური წლის",
-                DataSeederRepo.GetTableName<AcademicYear>(), nameof(AcademicYear.AyId),
-                nameof(AcademicYear.AcademicYearName), null,
+                DataSeederRepo.GetTableName<AcademicYear>(), nameof(AcademicYear.AyId), null,
+                nameof(AcademicYear.AcademicYearName), null, null, null,
                 GetDateOnlyCell(nameof(AcademicYear.StartDate), "სასწავლო წლის დასაწყისი"),
                 GetDateOnlyCell(nameof(AcademicYear.FinishDate), "სასწავლო წლის დასასრული")),
 
             //BankAccounts
             DataTypeFactory.Create("ბანკის ანგარიშები", "ბანკის ანგარიში", "ბანკის ანგარიშის",
-                DataSeederRepo.GetTableName<BankAccount>(), nameof(BankAccount.BaId), nameof(BankAccount.BankCode),
-                nameof(BankAccount.BankName), GetDateOnlyCell(nameof(BankAccount.AccountNumber), "ანგარიშის ნომერი"),
+                DataSeederRepo.GetTableName<BankAccount>(), nameof(BankAccount.BaId), null,
+                nameof(BankAccount.BankCode), null, nameof(BankAccount.BankName), null,
+                GetDateOnlyCell(nameof(BankAccount.AccountNumber), "ანგარიშის ნომერი"),
                 GetDateOnlyCell(nameof(BankAccount.DesperateDebt), "უიმედო ვალი")),
 
             //Courses
             DataTypeFactory.Create("საგნები", "საგანი", "საგნის", DataSeederRepo.GetTableName<Course>(),
-                nameof(Course.CrsId), nameof(Course.CourseName), null),
+                nameof(Course.CrsId), null, nameof(Course.CourseName), null, null, null),
 
             //CrmAnswerType
             DataTypeFactory.Create("მომხმარებლების პასუხების ტიპები", "მომხმარებლების პასუხის ტიპი",
                 "მომხმარებლების პასუხის ტიპი", DataSeederRepo.GetTableName<CrmAnswerType>(),
-                nameof(CrmAnswerType.CatId), nameof(CrmAnswerType.CatKey), nameof(CrmAnswerType.AnswerTypeName)),
+                nameof(CrmAnswerType.CatId), null, nameof(CrmAnswerType.CatKey), null,
+                nameof(CrmAnswerType.AnswerTypeName), null),
 
             //CrmCallType
             DataTypeFactory.Create("მომხმარებლებთან ზარების ტიპები", "მომხმარებლებთან ზარის ტიპი",
                 "მომხმარებლებთან ზარის ტიპის", DataSeederRepo.GetTableName<CrmCallType>(), nameof(CrmCallType.CctId),
-                nameof(CrmCallType.CallTypeName), null),
+                null, nameof(CrmCallType.CallTypeName), null, null, null),
 
             //ErrorLogTexts
             DataTypeFactory.Create("შეცდომების ლოგების ტექსტები", "შეცდომის ლოგის ტექსტი", "შეცდომის ლოგის ტექსტის",
-                DataSeederRepo.GetTableName<ErrorLogText>(), nameof(ErrorLogText.EltId), nameof(ErrorLogText.Text),
-                null),
+                DataSeederRepo.GetTableName<ErrorLogText>(), nameof(ErrorLogText.EltId), null,
+                nameof(ErrorLogText.Text), null, null, null),
 
             //GeoMonths
             DataTypeFactory.Create("თვეები", "თვე", "თვის", DataSeederRepo.GetTableName<GeoMonth>(),
-                nameof(GeoMonth.GmnId), nameof(GeoMonth.GmnName), nameof(GeoMonth.GmnDative)),
+                nameof(GeoMonth.GmnId), null, nameof(GeoMonth.GmnName), null, nameof(GeoMonth.GmnDative), null),
 
             //Groups
             DataTypeFactory.Create("ჯგუფები", "ჯგუფი", "ჯგუფის", DataSeederRepo.GetTableName<Group>(),
-                nameof(Group.GrpId), nameof(Group.GroupCode), null,
+                nameof(Group.GrpId), null, nameof(Group.GroupCode), null, null, null,
                 GetMdComboCell(nameof(Group.CourseId), "საგანი", DataSeederRepo.GetTableName<Course>()),
                 GetMdComboCell(nameof(Group.GroupSizeId), "ჯგუფის ზომა", DataSeederRepo.GetTableName<GroupSize>()),
                 GetMdComboCell(nameof(Group.StudentStatusId), "მოსწავლის სტატუსი",
@@ -77,8 +79,8 @@ public sealed class MimNewDataTypesSeeder : MimDataTypesSeeder
 
             //GroupSizes
             DataTypeFactory.Create("ჯგუფების ზომები", "ჯგუფის ზომა", "ჯგუფის ზომის",
-                DataSeederRepo.GetTableName<GroupSize>(), nameof(GroupSize.GrsId), nameof(GroupSize.GrsSize),
-                nameof(GroupSize.GrsName))
+                DataSeederRepo.GetTableName<GroupSize>(), nameof(GroupSize.GrsId), null, nameof(GroupSize.GrsSize),
+                null, nameof(GroupSize.GrsName), null)
         };
 
         mn.AddRange(newDataTypes);
