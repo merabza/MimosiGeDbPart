abc254f2ef211ac9135f1f751bb1f535a2796da2
merabza <merabza@gmail.com>
2026-01-02 21:10:42 +0400

    202601022110

---
+++ MimosiGeDb/DependencyInjection/MimosiGeDatabaseInstaller.cs
@@ -0,0 +1,34 @@
+//Created by DatabaseInstallerClassCreator at 2/15/2025 11:07:44 AM
+
+using System;
+using CarcassDb;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MimosiGeDb.DependencyInjection;
+
+// ReSharper disable once UnusedType.Global
+public static class MimosiGeDbDependencyInjection
+{
+    public static IServiceCollection AddMimosiGeDb(this IServiceCollection services, IConfiguration configuration,
+        bool debugMode)
+    {
+        const string connectionStringConfigurationKey = "Data:MimosiGeDatabase:ConnectionString";
+        if (debugMode) Console.WriteLine($"{nameof(AddMimosiGeDb)} Started");
+
+        var connectionString = configuration[connectionStringConfigurationKey];
+
+        if (string.IsNullOrWhiteSpace(connectionString) && !debugMode)
+        {
+            Console.WriteLine($"{connectionStringConfigurationKey} is empty");
+            return services;
+        }
+
+        services.AddDbContext<CarcassDbContext>(options => options.UseSqlServer(connectionString));
+        services.AddDbContext<MimosiGeDbContext>(options => options.UseSqlServer(connectionString));
+        if (debugMode) Console.WriteLine($"{nameof(AddMimosiGeDb)} Finished");
+
+        return services;
+    }
+}
